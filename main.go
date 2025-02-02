81b210635b590c74c63420fe453d14aa0919bd77
Gianluca Turra <gianlucaturra1@gmail.com>
2025-02-02 22:08:32 +0100

    Base

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package main
 
-import "github.com/GianlucaTurra/todoCli/cmd"
+import (
+	"database/sql"
+	"log"
+
+	"github.com/GianlucaTurra/todoCli/cmd"
+	_ "github.com/mattn/go-sqlite3"
+)
 
 func main() {
+	db, err := sql.Open("sqlite3", "./todo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	sqlStatement := `
+	create table if not exists todos (
+		id integer not null primary key autoincrement,
+		name text not null,
+		compleated integer not null default 0
+	)
+	`
+
+	_, err = db.Exec(sqlStatement)
+	if err != nil {
+		log.Printf("%q: %s\n", err, sqlStatement)
+		return
+	}
+
 	cmd.Execute()
 }
